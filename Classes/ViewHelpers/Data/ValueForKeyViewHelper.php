0d490a5090568438064e269b8796e3d25afc7f96
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2016-02-03 16:15:43 +0100

    Cast object to array

--- Classes/ViewHelpers/Data/ValueForKeyViewHelper.php
+++ Classes/ViewHelpers/Data/ValueForKeyViewHelper.php
@@ -41,7 +41,7 @@ class ValueForKeyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVie
 	 */
 	public function initializeArguments() {
 		parent::initializeArguments();
-		$this->registerArgument('array', 'array', 'The array to extract the value from', TRUE);
+		$this->registerArgument('array', 'mixed', 'The array to extract the value from', TRUE);
 		$this->registerArgument('key', 'string', 'The key to extract the value for', TRUE);
 	}
 
@@ -54,6 +54,11 @@ class ValueForKeyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVie
 		$result = NULL;
 
 		if ($this->arguments['array']) {
+
+			if(is_object($this->arguments['array'])) {
+				$this->arguments['array'] = (array)$this->arguments['array'];
+			}
+
 			if (array_key_exists($this->arguments['key'], $this->arguments['array'])) {
 				$result = $this->arguments['array'][$this->arguments['key']];
 			}
