11d5bff9e5220709e050f545bcdb4b670606529c
Christof Rodejohann <christof.rodejohann@slub-dresden.de>
2018-06-04 16:25:15 +0200

    Version hoch

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Find',
 	'description' => 'A frontend for Solr indexes',
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'state' => 'stable',
 	'category' => 'frontend',
 	'shy' => 0,
